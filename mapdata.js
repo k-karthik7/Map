9ebbdeda3c55cb7148028723b7688f368166fd14
KARTHIK K <74089000+k-karthik7@users.noreply.github.com>
2023-03-28 12:49:37 +0530

    commit

---
+++ mapdata.js
@@ -0,0 +1,333 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "gray",
+    background_transparent: "no",
+    border_color: "black",
+    pop_ups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    // state_url: "https://mrcet.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "black",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "/states/andaman_nicobar.html"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      description: "default",
+      color: "green",
+      hover_color: "default",
+      url: "default"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "4": {
+      name: "Assam",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "5": {
+      name: "Bihar",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "6": {
+      name: "Chandigarh",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "9": {
+      name: "Daman And Diu",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "10": {
+      name: "Delhi",
+      description: "default",
+      color: "#FF671F",
+      hover_color: "#FF671F",
+      url: "default"
+    },
+    "11": {
+      name: "Goa",
+      description: "default",
+      color: "green",
+      hover_color: "default",
+      url: "default"
+    },
+    "12": {
+      name: "Gujarat",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "13": {
+      name: "Haryana",
+      description: "default",
+      color: "#FF671F",
+      hover_color: "#FF671F",
+      url: "default"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      description: "default",
+      color: "#FF671F",
+      hover_color: "#FF671F",
+      url: "default"
+    },
+    "16": {
+      name: "Jharkhand",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "17": {
+      name: "Karnataka",
+      description: "default",
+      color: "green",
+      hover_color: "default",
+      url: "default"
+    },
+    "18": {
+      name: "Kerala",
+      description: "default",
+      color: "green",
+      hover_color: "default",
+      url: "default"
+    },
+    "19": {
+      name: "Lakshadweep",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "21": {
+      name: "Maharashtra",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "22": {
+      name: "Manipur",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "23": {
+      name: "Meghalaya",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "24": {
+      name: "Mizoram",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "25": {
+      name: "Nagaland",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "26": {
+      name: "Orissa",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "27": {
+      name: "Puducherry",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    "28": {
+      name: "Punjab",
+      description: "default",
+      color: "#FF671F",
+      hover_color: "#FF671F",
+      url: "default"
+    },
+    "29": {
+      name: "Rajasthan",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "30": {
+      name: "Sikkim",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      description: "default",
+      color: "green",
+      hover_color: "default",
+      url: "default"
+    },
+    "32": {
+      name: "Tripura",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "34": {
+      name: "Uttaranchal",
+      description: "default",
+      color: "#FF671F",
+      hover_color: "#FF671F",
+      url: "default"
+    },
+    "35": {
+      name: "West Bengal",
+      description: "default",
+      color: "white",
+      hover_color: "white",
+      url: "default"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      description: "default",
+      color: "#FF671F",
+      hover_color: "#FF671F",
+      url: "default"
+    },
+    "37": {
+      name: "Telangana",
+      description: "default",
+      color: "green",
+      hover_color: "default",
+      url: "/states/telangana.html",
+      url_new_tab: "/states/telangana.html",
+    },
+    "38": {
+      name: "Ladakh",
+      description: "default",
+      color: "#FF671F",
+      hover_color: "#FF671F",
+      url: "default"
+    }
+  },
+  labels: {}
+};
